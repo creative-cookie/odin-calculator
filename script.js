067c22881d407a35d6e7ddfa574c18958036ae2e
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-08-04 19:39:06 -0400

    add event listener to number buttons

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-const body = document.querySelector("body");
-const themeIcon = document.getElementById("theme-icon");
-
-themeIcon.addEventListener("click", toggleTheme);
+// Dark Mode Functionality
+document.getElementById("theme-icon").addEventListener("click", toggleTheme);
 
 function toggleTheme(){
-    body.classList.contains("darkmode") ? body.classList.remove("darkmode") : body.classList.add("darkmode");
+    document.body.classList.contains("darkmode") ? document.body.classList.remove("darkmode") : document.body.classList.add("darkmode");
     document.getElementById("moon").classList.toggle("hidden");
     document.getElementById("sun").classList.toggle("hidden");
 }
+
+//Calculator Object
+let calculator = {
+    total: "",
+    enterNumber(e){
+        this.total += "" + e.target.innerText;
+    },
+    outputTotal(){
+        if(this.total.toString().length <= 15){
+            document.getElementById("total").innerText = parseInt(this.total).toLocaleString();
+        }
+    }
+
+}
+
+//Number Button Functionality
+document.querySelectorAll(".btn--num").forEach((btn) => {
+    btn.addEventListener("click", (e)=>{
+        calculator.enterNumber(e);
+        calculator.outputTotal();
+})})
+
+
